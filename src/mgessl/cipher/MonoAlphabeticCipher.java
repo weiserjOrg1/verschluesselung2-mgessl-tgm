298fa8d87463cffdd70f7752c239e8603968215c
mgessl <mgessl@student.tgm.ac.at>
2018-10-12 13:13:04 +0200

    Erstellen der Datein

---
+++ src/mgessl/cipher/MonoAlphabeticCipher.java
@@ -0,0 +1,78 @@
+package mgessl.cipher;
+
+/**
+ * Klasse erbt von Interface Cipher. Die Klasse implementiert Klassen zum verschlüsseln von Texten
+ * @author micha
+ * @version 11.10.28
+ */
+public class MonoAlphabeticCipher implements Cipher{
+	private String secretAlphabet;
+	private String alphabet;
+	
+	public MonoAlphabeticCipher() {
+		setAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜß");
+	}
+	/**
+	 * Set für das normale Alphabet
+	 * @param String
+	 */
+	private void setAlphabet(String text) {
+		this.alphabet=text;
+	}
+	/**
+	 * Set für das SecretAlphabet
+	 * @param text
+	 * @return boolean
+	 */
+	protected boolean setSecretAlphabet(String text){
+		if (text.length()==30){
+			for (int i=0;i<text.length();i++) {
+				for (int j=0; j<text.length();j++) {
+					if(i != j) {
+						if(text.charAt(j) == text.charAt(i)) {
+							System.out.println("Ein Buchstabe ist doppelt");
+							return false;
+						}
+					}
+					
+				}
+			}
+			for(int i = 0;i<alphabet.length();i++) {
+				if(text.indexOf(alphabet.charAt(i))==-1) {
+					System.out.println("Ein Buchstabe ist nicht erlaubt");
+					return false;
+				}
+			}
+		} else {
+			System.out.println("Buchstaben fehlen");
+			return false;
+		}
+		this.secretAlphabet=text;
+		return true;
+	}
+	
+	/**
+	 * Verschlüsselt einen text
+	 * @param text zum verschlüsseln
+	 * @return String
+	 */
+	public String encrypt(String text) {
+		StringBuilder x = new StringBuilder();
+		for(int i = 0;i<text.length();i++) {
+			x.append(secretAlphabet.charAt(alphabet.indexOf(text.toUpperCase().charAt(i))));
+		}
+		return x.toString();
+	}
+	/**
+	 * Entschlüsselt einen text
+	 * @param text zum entschlüsseln
+	 * @return String
+	 */
+	public String decrypt(String text) {
+		StringBuilder x = new StringBuilder();
+		for(int i = 0;i<text.length();i++) {
+			x.append(alphabet.charAt(secretAlphabet.indexOf(text.toUpperCase().charAt(i))));
+		}
+		return x.toString();
+	}
+}
---
+++ src/mgessl/cipher/SubstitutionCipher.java
@@ -0,0 +1,27 @@
+package mgessl.cipher;
+
+/**
+ * Klasse erbt von MonoAlphabeticChiper. Verwendet die Methoden dieser Klasse.
+ * @author micha
+ * @version 11.10.28
+ */
+public class SubstitutionCipher extends MonoAlphabeticCipher{
+	public SubstitutionCipher(String secretAlphabet){
+		if (setSecretAlphabet(secretAlphabet)) {
+			String text = this.encrypt("MichaelGessl");
+			System.out.println(text);
+			System.out.println(this.decrypt(text));
+		}
+	}
+	/**
+	 * Set für das SecretAlphabet
+	 * @param text
+	 * @return boolean
+	 */
+	public boolean setSecretAlphabet(String secretAlphabet){
+		return super.setSecretAlphabet(secretAlphabet);
+	}
+	public static void main(String[] args) {
+		SubstitutionCipher sC = new SubstitutionCipher("QWERTZUIOPÜASDFGHJKLÖÄYXCVBNMß");
+	}
+}
